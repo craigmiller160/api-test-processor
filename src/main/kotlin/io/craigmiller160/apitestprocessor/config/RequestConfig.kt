737685e4452956e492760de5567516a2f65d46d8
Craig Miller <craigmiller160@gmail.com>
2020-09-30 18:21:59 -0400

    Missed one function that needed java interoperability

--- src/main/kotlin/io/craigmiller160/apitestprocessor/config/RequestConfig.kt
+++ src/main/kotlin/io/craigmiller160/apitestprocessor/config/RequestConfig.kt
@@ -2,12 +2,17 @@ package io.craigmiller160.apitestprocessor.config
 
 import io.craigmiller160.apitestprocessor.body.Body
 import org.springframework.http.HttpMethod
+import java.util.function.Consumer
 
 class RequestConfig {
     var method: HttpMethod = HttpMethod.GET
     var path: String = ""
     var body: Body? = null
     internal var overrideAuth: AuthConfig? = null
+
+    fun overrideAuth(init: Consumer<AuthConfig>) {
+        overrideAuth { init.accept(this) }
+    }
 
     fun overrideAuth(init: AuthConfig.() -> Unit) {
         overrideAuth = AuthConfig()
