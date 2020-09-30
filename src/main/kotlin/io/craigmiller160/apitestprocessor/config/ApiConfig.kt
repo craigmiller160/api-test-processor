f85078adcfd6520648e4fd01e018aaa4d9c65da2
Craig Miller <craigmiller160@gmail.com>
2020-09-30 18:15:51 -0400

    Improved more functions to be more java compatible

--- src/main/kotlin/io/craigmiller160/apitestprocessor/config/ApiConfig.kt
+++ src/main/kotlin/io/craigmiller160/apitestprocessor/config/ApiConfig.kt
@@ -1,13 +1,23 @@
 package io.craigmiller160.apitestprocessor.config
 
+import java.util.function.Consumer
+
 class ApiConfig {
     internal var req = RequestConfig()
     internal var res = ResponseConfig()
+
+    fun request(init: Consumer<RequestConfig>) {
+        request { init.accept(this) }
+    }
 
     fun request(init: RequestConfig.() -> Unit) {
         val request = RequestConfig()
         request.init()
         req = request
+    }
+
+    fun response(init: Consumer<ResponseConfig>) {
+        response { init.accept(this) }
     }
 
     fun response(init: ResponseConfig.() -> Unit) {
